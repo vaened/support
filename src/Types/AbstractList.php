50a15da148b7008646d4431b7197e49c967ccda8
Enea Dhack <enea.so@live.com>
2023-08-10 17:52:55 -0500

    refactor: improve

--- src/Types/AbstractList.php
+++ src/Types/AbstractList.php
@@ -16,25 +16,15 @@ use function array_reverse;
 use function array_values;
 use function count;
 use function iterator_to_array;
-use function Lambdish\Phunctional\each;
-use function Lambdish\Phunctional\filter;
-use function Lambdish\Phunctional\flatten;
-use function Lambdish\Phunctional\map;
-use function Lambdish\Phunctional\reduce;
-use function Lambdish\Phunctional\some;
+use function Lambdish\Phunctional\{each, filter, flatten, map, reduce, some};
 
-class AbstractList implements Countable, IteratorAggregate
+abstract class AbstractList implements Countable, IteratorAggregate
 {
     protected array $items;
 
     public function __construct(iterable $items)
     {
-        $this->items = match (true) {
-            $items instanceof self => $items->items(),
-            $items instanceof Traversable => iterator_to_array($items),
-            $items instanceof ArrayIterator => $items->getArrayCopy(),
-            default => $items,
-        };
+        $this->items = self::parse($items);
     }
 
     public function merge(self $list): static
@@ -45,7 +35,7 @@ class AbstractList implements Countable, IteratorAggregate
         ]);
     }
 
-    public function overlay(AbstractList $list): static
+    public function overlay(self $list): static
     {
         $items = $this->items();
 
@@ -61,14 +51,14 @@ class AbstractList implements Countable, IteratorAggregate
         return new static(array_reverse($this->items, true));
     }
 
-    public function flatMap(callable $callback): iterable
+    public function flatMap(callable $operation): iterable
     {
-        return flatten($this->map($callback));
+        return flatten($this->map($operation));
     }
 
-    public function map(callable $callback): iterable
+    public function map(callable $predicate): iterable
     {
-        return map($callback, $this->items());
+        return map($predicate, $this->items());
     }
 
     public function pick(callable $predicate): mixed
@@ -82,25 +72,25 @@ class AbstractList implements Countable, IteratorAggregate
         return null;
     }
 
-    public function each(callable $callback): static
+    public function each(callable $operation): static
     {
-        each($callback, $this->items());
+        each($operation, $this->items());
         return $this;
     }
 
-    public function filter(callable $callback): static
+    public function filter(callable $predicate): static
     {
-        return new static(filter($callback, $this->items()));
+        return new static(filter($predicate, $this->items()));
     }
 
-    public function reduce(callable $callback, mixed $initial = null): mixed
+    public function reduce(callable $operation, mixed $initial = null): mixed
     {
-        return reduce($callback, $this->items(), $initial);
+        return reduce($operation, $this->items(), $initial);
     }
 
-    public function some(callable $callback): bool
+    public function some(callable $predicate): bool
     {
-        return some($callback, $this->items());
+        return some($predicate, $this->items());
     }
 
     public function keyOf(callable $criteria): int|string|null
@@ -137,5 +127,15 @@ class AbstractList implements Countable, IteratorAggregate
     public function getIterator(): Traversable
     {
         return new ArrayIterator($this->items());
+    }
+
+    private static function parse(iterable $items): array
+    {
+        return match (true) {
+            $items instanceof self => $items->items(),
+            $items instanceof Traversable => iterator_to_array($items),
+            $items instanceof ArrayIterator => $items->getArrayCopy(),
+            default => $items,
+        };
     }
 }
